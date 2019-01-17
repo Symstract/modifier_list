89a5653f5e931243b1529e2e81aa58e805c60a77
Antti Tikka <antti.tikka@hotmail.com>
2019-01-17 04:11:44 +0200

    Cleanup: Add/Remove blank lines, whitespaces

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ import math
 import numpy as np
 
 from bl_ui.properties_data_modifier import DATA_PT_modifiers
-import bpy 
+import bpy
 from bpy.app.handlers import persistent
 from bpy.props import *
 from bpy.types import (
@@ -50,7 +50,7 @@ import rna_keymap_ui
 
 class Preferences(AddonPreferences):
     bl_idname = __name__
-    
+
     modifier_01 = StringProperty()
     modifier_02 = StringProperty()
     modifier_03 = StringProperty()
@@ -64,7 +64,6 @@ class Preferences(AddonPreferences):
     modifier_11 = StringProperty()
     modifier_12 = StringProperty()
 
-
     def draw(self, context):
         layout = self.layout
 
@@ -75,9 +74,9 @@ class Preferences(AddonPreferences):
 
         num_of_mods = len(get_pref_attr_name())
         num_of_rows = math.ceil(num_of_mods / 2)
-        
+
         attr_iter = iter(get_pref_attr_name())
-        
+
         wm = bpy.context.window_manager
 
         # Draw two property searches per row
@@ -101,11 +100,12 @@ class Preferences(AddonPreferences):
 
 #=======================================================================
 
-    
+
 def get_pref_attr_name():
     """For drawing favourite modifier selection rows in preferences."""
 
     attr_name_list = [attr for attr in dir(Preferences) if "modifier_" in attr]
+
     return attr_name_list
 
 
@@ -116,7 +116,7 @@ def get_pref_attr_value():
     # get correct class attributes and then their values
     attr_list = [attr for attr in dir(fav_mods) if "modifier_" in attr]
     attr_value_list = [getattr(fav_mods, attr) for attr in attr_list]
-    
+
     return attr_value_list
 
 
@@ -124,13 +124,13 @@ def all_name_icon_type():
     """List of all modifier names, icons and types."""
 
     mods_enum = bpy.types.Modifier.bl_rna.properties['type'].enum_items
-    
+
     all_mod_names = [modifier.name for modifier in mods_enum]
     all_mod_icons = [modifier.icon for modifier in mods_enum]
     all_mod_types = [modifier.identifier for modifier in mods_enum]
 
     all_mods_zipped = list(zip(all_mod_names, all_mod_icons, all_mod_types))
-    
+
     return all_mods_zipped
 
 
@@ -138,16 +138,12 @@ def fav_name_icon_type():
     """Iterator of favourite modifier names, icons and types."""
 
     mods_enum = bpy.types.Modifier.bl_rna.properties['type'].enum_items
-
     all_mod_names = [modifier.name for modifier in mods_enum]
-
     all_mods_dict = dict(zip(all_mod_names, all_name_icon_type()))
-
     fav_mods_list = [all_mods_dict[mod] if mod in all_mods_dict else (None, None, None)
                      for mod in get_pref_attr_value()]
-
     fav_mods_iter = iter(fav_mods_list)
-    
+
     return fav_mods_iter
 
 
@@ -157,8 +153,8 @@ def fav_name_icon_type():
 class AllModifiersCollection(PropertyGroup):
     # Collection Property for search
     value = StringProperty(name="my modifier")
-    
- 
+
+
 def add_modifier(self, context):
     # Add modifier
     wm = bpy.context.window_manager
@@ -171,7 +167,7 @@ def add_modifier(self, context):
     mods = ob.modifiers
     mods_len = len(mods) - 1
     ob.modifier_active_index = mods_len
-    
+
     # Executing an operator via function doesn't create an undo event,
     # so it needs to be added manually.
     bpy.ops.ed.undo_push(message="Add Modifier")
@@ -184,12 +180,13 @@ class OBJECT_MT_custom_add_modifier_menu(Menu):
 
     def draw(self, context):
         layout = self.layout
+
         row = layout.row()
         row.alignment = 'LEFT'
 
         col = row.column()
         col.label(text="Modify")
-        
+
         sub = col.column()
         sub.scale_y = 0.3
         sub.separator()
@@ -199,7 +196,7 @@ class OBJECT_MT_custom_add_modifier_menu(Menu):
 
         col = row.column()
         col.label(text="Generate")
-        
+
         sub = col.column()
         sub.scale_y = 0.3
         sub.separator()
@@ -209,7 +206,7 @@ class OBJECT_MT_custom_add_modifier_menu(Menu):
 
         col = row.column()
         col.label(text="Deform")
-        
+
         sub = col.column()
         sub.scale_y = 0.3
         sub.separator()
@@ -228,7 +225,7 @@ class OBJECT_MT_custom_add_modifier_menu(Menu):
 
 
 class MODIFIERS_UL_modifier_list(UIList):
-    
+
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
         mod = item
 
@@ -236,16 +233,16 @@ class MODIFIERS_UL_modifier_list(UIList):
             if mod:
                 layout.label(text="", translate=False, icon_value=layout.icon(mod))
                 layout.prop(mod, "name", text="", emboss=False, icon_value=icon)
-                
+
                 # Hide visibility toggles for collision modifier as they are not used
                 # in the regular UI either (apparently can cause problems in some scenes)
                 if mod.type != 'COLLISION':
                     icon = 'RESTRICT_VIEW_OFF' if mod.show_viewport else 'RESTRICT_VIEW_ON'
                     layout.prop(mod, "show_viewport", text="", icon=icon, emboss=False)
-                    
+
                     icon = 'RESTRICT_RENDER_OFF' if mod.show_render else 'RESTRICT_RENDER_ON'
                     layout.prop(mod, "show_render", text="", icon=icon, emboss=False)
-                
+
                 icon = 'EDITMODE_HLT' if mod.show_in_editmode else 'OBJECT_DATAMODE'
                 layout.prop(mod, "show_in_editmode", text="", icon=icon, emboss=False)
 
@@ -253,12 +250,12 @@ class MODIFIERS_UL_modifier_list(UIList):
                 layout.prop(mod, "show_on_cage", text="", icon=icon, emboss=False)
             else:
                 layout.label(text="", translate=False, icon_value=icon)
-        
+
         elif self.layout_type in {'GRID'}:
             layout.alignment = 'CENTER'
             layout.label(text="", icon_value=icon)
 
-    
+
 class ModifierListActions(Operator):
     """Base operator for list actions."""
 
@@ -273,18 +270,18 @@ class ModifierListActions(Operator):
         active_mod_index = ob.modifier_active_index
         active_mod_index_up = np.clip(active_mod_index - 1, 0, mods_len)
         active_mod_index_down = np.clip(active_mod_index + 1, 0, mods_len)
-        
+
         if mods:
             active_mod_name = ob.modifiers[active_mod_index].name
 
             if self.action == 'UP':
                 bpy.ops.object.modifier_move_up(modifier=active_mod_name)
                 ob.modifier_active_index = active_mod_index_up
-            
+
             if self.action == 'DOWN':
                 bpy.ops.object.modifier_move_down(modifier=active_mod_name)
                 ob.modifier_active_index = active_mod_index_down
-            
+
             if self.action == 'REMOVE':
                 bpy.ops.object.modifier_remove(modifier=active_mod_name)
                 ob.modifier_active_index = active_mod_index_up
@@ -316,7 +313,7 @@ class OBJECT_OT_custom_modifier_remove(ModifierListActions):
     action = 'REMOVE'
 
 
-class OBJECT_OT_custom_modifier_add(Operator): 
+class OBJECT_OT_custom_modifier_add(Operator):
     bl_idname = "object.custom_modifier_add"
     bl_label = "Add Modifier"
     bl_description = "Add a procedural operation/effect to the active object"
@@ -326,7 +323,7 @@ class OBJECT_OT_custom_modifier_add(Operator):
 
     def execute(self, context):
         bpy.ops.object.modifier_add(type=self.modifier_type)
-        
+
         # Set correct active_mod index
         ob = context.object
         mods = ob.modifiers
@@ -348,7 +345,7 @@ class OBJECT_OT_custom_modifier_apply(Operator):
     apply_as = EnumProperty(
         items=(
             ('DATA', "Data", ""),
-            ('SHAPE', "Shape", "")), 
+            ('SHAPE', "Shape", "")),
         default='DATA')
 
 
@@ -375,7 +372,7 @@ class OBJECT_OT_custom_modifier_copy(Operator):
 
     def execute(self, context):
         bpy.ops.object.modifier_copy(modifier=self.modifier)
-        
+
         # Set correct active_mod index
         ob = context.object
         active_index = ob.modifier_active_index
@@ -394,7 +391,7 @@ class VIEW_3D_PT_modifier_popup(Operator):
     def execute(self, context):
         return {'FINISHED'}
 
-    def invoke(self, context, event):   
+    def invoke(self, context, event):
         return context.window_manager.invoke_popup(self, width=300)
 
     def check(self, context):
@@ -402,36 +399,35 @@ class VIEW_3D_PT_modifier_popup(Operator):
 
     def draw(self, context):
         layout = self.layout
-        
+
         ob = context.object
 
         if not ob:
             layout.label(text="No active object")
-        elif ob.type not in {'MESH', 'CURVE', 'SURFACE', 'FONT',  'LATTICE'}:
+        elif ob.type not in {'MESH', 'CURVE', 'SURFACE', 'FONT', 'LATTICE'}:
             layout.label(text="Wrong object type")
         else:
-
             # === Favourite modifiers ===
             col = layout.column(align=True)
 
             fav_name_icon_type_iter = fav_name_icon_type()
 
-            # Check if an item or the next item in fav_name_icon_type has a value 
+            # Check if an item or the next item in fav_name_icon_type has a value
             # and add rows and buttons accordingly (two buttons per row)
             for name, icon, mod in fav_name_icon_type_iter:
                 next_mod = next(fav_name_icon_type_iter)
                 if name or next_mod[0] is not None:
                     row = col.split(percentage=0.5, align=True)
-                    
+
                     if name is not None:
                         add_modifer = row.operator("object.custom_modifier_add",
                                                    text=name,
                                                    icon=icon).modifier_type = mod
                     else:
                         row.label(text="")
-                    
+
                     if next_mod[0] is not None:
-                        row.operator("object.custom_modifier_add", 
+                        row.operator("object.custom_modifier_add",
                                      text=next_mod[0],
                                      icon=next_mod[1]).modifier_type = next_mod[2]
                     else:
@@ -439,21 +435,22 @@ class VIEW_3D_PT_modifier_popup(Operator):
 
             # === Modifier search and menu ===
             wm = bpy.context.window_manager
+
             col = layout.column()
+
             row = col.split(percentage=0.65)
             row.prop_search(wm, "mod_to_add", wm, "all_modifiers", text="", icon='MODIFIER')
             row.menu("OBJECT_MT_custom_add_modifier_menu")
 
-
             # === Modifier list ===
             ob = context.object
 
-            layout.template_list("MODIFIERS_UL_modifier_list", "", ob, "modifiers", 
+            layout.template_list("MODIFIERS_UL_modifier_list", "", ob, "modifiers",
                                  ob, "modifier_active_index")
 
             # === Modifier tools (from the addon) ===
             row = layout.row()
-            
+
             sub = row.row(align=True)
             sub.scale_x = 2.0
             sub.operator("object.toggle_apply_modifiers_view", icon='RESTRICT_VIEW_OFF', text="")
@@ -464,7 +461,6 @@ class VIEW_3D_PT_modifier_popup(Operator):
             sub = row.row(align=True)
             sub.scale_x = 2.0
             sub.alignment = 'RIGHT'
-
             sub.operator(OBJECT_OT_custom_modifier_move_up.bl_idname, icon='TRIA_UP', text="")
             sub.operator(OBJECT_OT_custom_modifier_move_down.bl_idname, icon='TRIA_DOWN', text="")
             sub.operator(OBJECT_OT_custom_modifier_remove.bl_idname, icon='ZOOMOUT', text="")
@@ -472,20 +468,21 @@ class VIEW_3D_PT_modifier_popup(Operator):
             # === Modifier settings ===
             mp = DATA_PT_modifiers(context)
             ob = context.object
-            
+
             row = layout.row()
             if ob:
                 if ob.modifiers:
                     active_mod_index = ob.modifier_active_index
                     active_mod = ob.modifiers[active_mod_index]
 
-                    active_mod_icon = [icon for name, icon, mod in all_name_icon_type() 
+                    active_mod_icon = [icon for name, icon, mod in all_name_icon_type()
                                        if mod == active_mod.type].pop()
 
                     column = layout.column(align=True)
+
                     box = column.box()
                     row = box.row()
-                    
+
                     # === General settings ===
                     sub = row.row()
                     sub.label(text="", icon=active_mod_icon)
@@ -496,39 +493,36 @@ class VIEW_3D_PT_modifier_popup(Operator):
                     sub.alignment = 'RIGHT'
                     # Hide visibility toggles for collision modifier as they are not used
                     # in the regular UI either (apparently can cause problems in some scenes)
-                    if active_mod.type != 'COLLISION':              
+                    if active_mod.type != 'COLLISION':
                         sub.prop(active_mod, "show_viewport", text="")
                         sub.prop(active_mod, "show_render", text="")
                     sub.prop(active_mod, "show_in_editmode", text="")
                     sub.prop(active_mod, "show_on_cage", text="")
 
                     row = box.row()
-                    row.operator("object.custom_modifier_apply", 
+                    row.operator("object.custom_modifier_apply",
                                  text="Apply").modifier = active_mod.name
 
                     sub = row.row()
-
-                    # Cloth and Soft Body have "Apply As Shape Key" but no "Copy Modifier" .
+                    # Cloth and Soft Body have "Apply As Shape Key" but no "Copy Modifier".
                     # In those cases "Apply As Shape Key" doesn't need to be scaled up.
                     if active_mod.type not in {'CLOTH', 'SOFT_BODY'}:
                         sub.scale_x = 1.3
-                    
                     deform_mods = {mod for name, icon, mod in all_name_icon_type()[25:42]}
                     other_shape_key_mods = {'CLOTH', 'SOFT_BODY', 'MESH_CACHE'}
                     has_shape_key = deform_mods.union(other_shape_key_mods)
-                    
                     if active_mod.type in has_shape_key:
-                        apply_as_shape_key = sub.operator("object.custom_modifier_apply", 
+                        apply_as_shape_key = sub.operator("object.custom_modifier_apply",
                                                           text="Apply as Shape Key")
                         apply_as_shape_key.modifier=active_mod.name
                         apply_as_shape_key.apply_as='SHAPE'
-                    
-                    has_no_copy = {'CLOTH', 'COLLISION', 'DYNAMIC_PAINT', 'FLUID_SIMULATION', 
+
+                    has_no_copy = {'CLOTH', 'COLLISION', 'DYNAMIC_PAINT', 'FLUID_SIMULATION',
                                    'PARTICLE_SYSTEM', 'SMOKE', 'SOFT_BODY'}
                     if active_mod.type not in has_no_copy:
-                        row.operator("object.custom_modifier_copy", 
+                        row.operator("object.custom_modifier_copy",
                                      text="Copy").modifier = active_mod.name
-                    
+
                     # === Modifier specific settings ===
                     box = column.box()
                     getattr(mp, active_mod.type)(box, ob, active_mod)
@@ -543,7 +537,7 @@ def set_modifier_collection_items():
     """
 
     all_modifiers = bpy.context.window_manager.all_modifiers
-    
+
     if not all_modifiers:
         for name, icon, mod in all_name_icon_type():
             item = all_modifiers.add()
@@ -555,7 +549,7 @@ def set_modifier_collection_items():
 def on_file_load(dummy):
     set_modifier_collection_items()
 
-       
+
 classes = (
     Preferences,
     AllModifiersCollection,
@@ -572,6 +566,7 @@ classes = (
 
 addon_keymaps = []
 
+
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
@@ -579,10 +574,10 @@ def register():
     bpy.types.Object.modifier_active_index = IntProperty()
 
     wm = bpy.types.WindowManager
-    wm.mod_to_add = StringProperty(name="Modifier to add", update=add_modifier, 
+    wm.mod_to_add = StringProperty(name="Modifier to add", update=add_modifier,
                                    description="Search for a modifier and add it to the stack")
     wm.all_modifiers = CollectionProperty(type=AllModifiersCollection)
-      
+
     bpy.app.handlers.load_post.append(on_file_load)
 
     set_modifier_collection_items()
@@ -596,6 +591,7 @@ def register():
         kmi.active = True
         addon_keymaps.append((km, kmi))
 
+
 def unregister():
     for km, kmi in addon_keymaps:
         km.keymap_items.remove(kmi)
@@ -603,9 +599,9 @@ def unregister():
 
     for cls in classes:
         bpy.utils.unregister_class(cls)
-    
-    bpy.app.handlers.load_post.remove(on_file_load) 
-    
+
+    bpy.app.handlers.load_post.remove(on_file_load)
+
     wm = bpy.context.window_manager
 
     del bpy.types.Object.modifier_active_index
