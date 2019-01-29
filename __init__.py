2714afef2dc542e55195b00551bbc3b39c05bae9
Antti Tikka <antti.tikka@hotmail.com>
2019-01-29 23:57:58 +0200

    Cleanup: Various formatting and other tweaks

--- __init__.py
+++ __init__.py
@@ -121,26 +121,21 @@ def get_pref_mod_attr_name():
     class for making drawing favourite modifier selection rows in
     preferences easy.
     """
-
     attr_name_list = [attr for attr in dir(Preferences) if "modifier_" in attr]
-
     return attr_name_list
 
 
 def get_pref_mod_attr_value():
     """List of the names of favourite modifiers"""
-
     prefs = bpy.context.user_preferences.addons[__name__].preferences
     # Get correct class attributes and then their values
     attr_list = [attr for attr in dir(prefs) if "modifier_" in attr]
     attr_value_list = [getattr(prefs, attr) for attr in attr_list]
-
     return attr_value_list
 
 
 def all_name_icon_type():
     """List of tuples of the names, icons and types of all modifiers."""
-
     mods_enum = bpy.types.Modifier.bl_rna.properties['type'].enum_items
 
     all_mod_names = [modifier.name for modifier in mods_enum]
@@ -148,7 +143,6 @@ def all_name_icon_type():
     all_mod_types = [modifier.identifier for modifier in mods_enum]
 
     all_mods_zipped = list(zip(all_mod_names, all_mod_icons, all_mod_types))
-
     return all_mods_zipped
 
 
@@ -156,14 +150,12 @@ def fav_name_icon_type():
     """Iterator of tuples of the names, icons and types of favourite
     modifiers.
     """
-
     mods_enum = bpy.types.Modifier.bl_rna.properties['type'].enum_items
     all_mod_names = [modifier.name for modifier in mods_enum]
     all_mods_dict = dict(zip(all_mod_names, all_name_icon_type()))
     fav_mods_list = [all_mods_dict[mod] if mod in all_mods_dict else (None, None, None)
                      for mod in get_pref_mod_attr_value()]
     fav_mods_iter = iter(fav_mods_list)
-
     return fav_mods_iter
 
 
@@ -180,16 +172,17 @@ def mod_show_editmode_and_cage(modifier, layout, scale_x=1.0, emboss=True):
     account but instead shows the button always in those cases. It's
     easier to achieve and hardly makes a difference.
     """
-
-    has_no_show_in_editmode = {'MESH_SEQUENCE_CACHE', 'BUILD', 'DECIMATE', 'MULTIRES', 'CLOTH', 'COLLISION',
-                           'DYNAMIC_PAINT','EXPLODE', 'FLUID_SIMULATION', 'PARTICLE_SYSTEM',
-                           'SMOKE', 'SOFT_BODY'}
+    has_no_show_in_editmode = {
+        'MESH_SEQUENCE_CACHE', 'BUILD', 'DECIMATE', 'MULTIRES', 'CLOTH', 'COLLISION',
+        'DYNAMIC_PAINT','EXPLODE', 'FLUID_SIMULATION', 'PARTICLE_SYSTEM','SMOKE', 'SOFT_BODY'
+    }
 
     deform_mods = {mod for name, icon, mod in all_name_icon_type()[25:41]}
-    other_show_on_cage_mods = {'DATA_TRANSFER', 'NORMAL_EDIT', 'WEIGHTED_NORMAL', 'UV_PROJECT',
-                           'VERTEX_WEIGHT_EDIT', 'VERTEX_WEIGHT_MIX', 'VERTEX_WEIGHT_PROXIMITY',
-                           'ARRAY', 'EDGE_SPLIT', 'MASK', 'MIRROR','SOLIDIFY', 'SUBSURF',
-                           'TRIANGULATE'}
+    other_show_on_cage_mods = {
+        'DATA_TRANSFER', 'NORMAL_EDIT', 'WEIGHTED_NORMAL', 'UV_PROJECT', 'VERTEX_WEIGHT_EDIT',
+        'VERTEX_WEIGHT_MIX', 'VERTEX_WEIGHT_PROXIMITY', 'ARRAY', 'EDGE_SPLIT', 'MASK', 'MIRROR',
+        'SOLIDIFY', 'SUBSURF', 'TRIANGULATE'
+    }
     has_show_on_cage = deform_mods.union(other_show_on_cage_mods)
 
     # === show_in_editmode ===
@@ -217,7 +210,7 @@ def mod_show_editmode_and_cage(modifier, layout, scale_x=1.0, emboss=True):
                 break
 
         # Check if some modifier after this has show_in_editmode and
-        # show_on_cage both on and also is visible in viewport.
+        # show_on_cage both on and also is visible in the viewport.
         is_after_show_on_cage_on = False
         for mod in mods[(mod_index + 1):(len(mods))]:
             if (mod.show_viewport and mod.show_in_editmode
@@ -258,7 +251,7 @@ def add_modifier(self, context):
     mods_len = len(mods) - 1
     ob.modifier_active_index = mods_len
 
-    # Executing an operator via function doesn't create an undo event,
+    # Executing an operator via a function doesn't create an undo event,
     # so it needs to be added manually.
     bpy.ops.ed.undo_push(message="Add Modifier")
 
@@ -436,8 +429,10 @@ class OBJECT_OT_custom_modifier_apply(Operator):
     apply_as = EnumProperty(
         items=(
             ('DATA', "Data", ""),
-            ('SHAPE', "Shape", "")),
-        default='DATA')
+            ('SHAPE', "Shape", "")
+        ),
+        default='DATA'
+    )
 
 
     def execute(self, context):
@@ -511,15 +506,13 @@ class VIEW_3D_PT_modifier_popup(Operator):
                     row = col.split(percentage=0.5, align=True)
 
                     if name is not None:
-                        add_modifer = row.operator("object.custom_modifier_add",
-                                                   text=name,
+                        add_modifer = row.operator("object.custom_modifier_add", text=name,
                                                    icon=icon).modifier_type = mod
                     else:
                         row.label(text="")
 
                     if next_mod[0] is not None:
-                        row.operator("object.custom_modifier_add",
-                                     text=next_mod[0],
+                        row.operator("object.custom_modifier_add", text=next_mod[0],
                                      icon=next_mod[1]).modifier_type = next_mod[2]
                     else:
                         row.label(text="")
@@ -607,8 +600,10 @@ class VIEW_3D_PT_modifier_popup(Operator):
                         apply_as_shape_key.modifier=active_mod.name
                         apply_as_shape_key.apply_as='SHAPE'
 
-                    has_no_copy = {'CLOTH', 'COLLISION', 'DYNAMIC_PAINT', 'FLUID_SIMULATION',
-                                   'PARTICLE_SYSTEM', 'SMOKE', 'SOFT_BODY'}
+                    has_no_copy = {
+                        'CLOTH', 'COLLISION', 'DYNAMIC_PAINT', 'FLUID_SIMULATION',
+                        'PARTICLE_SYSTEM', 'SMOKE', 'SOFT_BODY'
+                    }
                     if active_mod.type not in has_no_copy:
                         row.operator("object.custom_modifier_copy",
                                      text="Copy").modifier = active_mod.name
@@ -629,7 +624,6 @@ def set_modifier_collection_items():
     """This is to be called on loading a new file or reloading addons
     to make modifiers available in search.
     """
-
     all_modifiers = bpy.context.window_manager.all_modifiers
 
     if not all_modifiers:
