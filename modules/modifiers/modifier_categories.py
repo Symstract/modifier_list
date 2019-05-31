d56957004234053a26bb1e759304ff515f2c4d8a
Antti Tikka <antti.tikka@hotmail.com>
2019-05-31 17:30:37 +0300

    Add operator for adding gizmo object to modifier

---
+++ modules/modifiers/modifier_categories.py
@@ -0,0 +1,11 @@
+have_gizmo_property = {
+    'NORMAL_EDIT': "target",
+    'VERTEX_WEIGHT_PROXIMITY': "target",
+    'ARRAY': "offset_object",
+    'MIRROR': "mirror_object",
+    'SCREW': "object",
+    'CAST': "object",
+    'HOOK': "object",
+    'SIMPLE_DEFORM': "origin",
+    'WAVE': "start_position_object"
+}
--- modules/modifiers/modifiers_ui.py
+++ modules/modifiers/modifiers_ui.py
@@ -13,7 +13,7 @@ from bpy.types import (
     UIList
 )
 
-from . import ml_modifier_layouts
+from . import ml_modifier_layouts, modifier_categories
 from .. import icons
 
 
@@ -161,6 +161,81 @@ def mod_show_editmode_and_cage(modifier, layout, scale_x=1.0, use_in_list=False)
         # Make icons align nicely
         empy_icon = pcoll['EMPTY_SPACE']
         sub.label(text="", translate=False, icon_value=empy_icon.icon_id)
+
+
+# Functions for adding a gizmo object
+# ======================================================================
+
+def _get_ml_collection(context):
+    """Get the ml gizmo collection or create it if it doesnt exist yet"""
+    scene = context.scene
+
+    if "ML_Gizmo Objects" not in scene.collection.children:
+        ml_col = bpy.data.collections.new("ML_Gizmo Objects")
+        scene.collection.children.link(ml_col)
+    else:
+        ml_col = bpy.data.collections["ML_Gizmo Objects"]
+
+    return ml_col
+
+
+def _create_gizmo_object(self, context, modifier, place_at_vertex):
+    """Create a gizmo (empty) object"""
+    ob = context.object
+    ob.update_from_editmode()
+    ob_mat = ob.matrix_world
+    mesh = ob.data
+
+    sel_verts = [v for v in mesh.vertices if v.select]
+
+    if place_at_vertex :
+        if len(sel_verts) != 1:
+            self.report(type={'INFO'}, message="Please, select (only) a single vertex")
+            return
+        else:
+            vert_loc = ob_mat @ sel_verts[0].co
+
+    gizmo_ob = bpy.data.objects.new(modifier + "_Gizmo Object", None)
+
+    if place_at_vertex:
+        gizmo_ob.location = vert_loc
+    else:
+        gizmo_ob.location = ob_mat.to_translation()
+
+    ml_col = _get_ml_collection(context)
+    ml_col.objects.link(gizmo_ob)
+
+    return gizmo_ob
+
+
+def assign_gizmo_object_to_modifier(self, context, modifier, place_at_vertex=False):
+    """Assign a gizmo object to the correct property of the given modifier"""
+    ob = context.object
+    mod = ob.modifiers[modifier]
+
+    # If modifier is UV Project, handle it differently here
+    if mod.type == 'UV_PROJECT':
+        projectors = ob.modifiers[modifier].projectors
+        projector_count = ob.modifiers[modifier].projector_count
+
+        for p in projectors[0:projector_count]:
+            if not p.object:
+                gizmo_ob = _create_gizmo_object(self, context, modifier,
+                                             place_at_vertex=place_at_vertex)
+                p.object = gizmo_ob
+                break
+
+        return
+
+    # If modifier is not UV Project, continue normally
+    gizmo_ob = _create_gizmo_object(self, context, modifier, place_at_vertex=place_at_vertex)
+
+    if mod.type == 'ARRAY':
+        mod.use_object_offset = True
+
+    gizmo_ob_prop = modifier_categories.have_gizmo_property[mod.type]
+
+    setattr(mod, gizmo_ob_prop, gizmo_ob)
 
 
 # Modifier list and operator classes etc.
@@ -397,6 +472,28 @@ class OBJECT_OT_ml_modifier_copy(Operator):
         return {'FINISHED'}
 
 
+class OBJECT_OT_ml_create_gizmo_object(Operator):
+    bl_idname = "object.ml_create_gizmo_object"
+    bl_label = "Add Gizmo Object"
+    bl_description = ("Add a gizmo object to the modifier.\n"
+                      "\n"
+                      "\u2022 Click to place it at the origin of the active object.\n"
+                      "\u2022 Click + Hold shift to place it at the selected vertex")
+    bl_options = {'REGISTER', 'INTERNAL', 'UNDO'}
+
+    modifier: StringProperty()
+
+    def execute(self, context):
+        assign_gizmo_object_to_modifier(self, context, self.modifier, self.place_at_vertex)
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        self.place_at_vertex = True if event.shift else False
+
+        return self.execute(context)
+
+
 # UI
 #=======================================================================
 
@@ -523,6 +620,13 @@ def modifiers_ui(context, layout, num_of_rows=False):
             if active_mod.type not in has_no_copy:
                 row.operator("object.ml_modifier_copy",
                              text="Copy").modifier = active_mod.name
+
+            # === Gizmo object settings ===
+            if (active_mod.type in modifier_categories.have_gizmo_property
+                    or active_mod.type == 'UV_PROJECT'):
+                box = col.box()
+                box.operator("object.ml_create_gizmo_object", text="", icon='EMPTY_DATA'
+                            ).modifier = active_mod.name
 
             # === Modifier specific settings ===
             box = col.box()
