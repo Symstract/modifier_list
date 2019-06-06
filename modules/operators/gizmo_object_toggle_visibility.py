63219947cbd3c22aa9a550b63307fb02b61e665e
Antti Tikka <antti.tikka@hotmail.com>
2019-06-06 16:20:03 +0300

    Revert "Cleanup: Capital letters in operator description"
    This reverts commit c891c88141c8dfd379b9ee87b3194345660c01fa.

--- modules/operators/gizmo_object_toggle_visibility.py
+++ modules/operators/gizmo_object_toggle_visibility.py
@@ -4,14 +4,14 @@ from ..modifier_categories import have_gizmo_property
 from ..utils import get_gizmo_object
 
 
-class OBJECT_OT_ml_gizmo_object_(Operator):
+class OBJECT_OT_ml_gizmo_object_toggle_visibility(Operator):
     """The purpose of this operator is to get the opposite behaviour
     for the UI button to what the behaviour would be using the
     hide_viewport property.
     """
     bl_idname = "object.ml_gizmo_object_toggle_visibility"
     bl_label = "Toggle Gizmo Object Visibility"
-    bl_description = "Toggle gizmo object visibility"
+    bl_description = "Toggle Gizmo Object Visibility"
     bl_options = {'REGISTER', 'INTERNAL', 'UNDO'}
 
     def execute(self, context):
