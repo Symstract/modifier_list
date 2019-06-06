c891c88141c8dfd379b9ee87b3194345660c01fa
Antti Tikka <antti.tikka@hotmail.com>
2019-06-06 15:29:46 +0300

    Cleanup: Capital letters in operator description

--- modules/operators/gizmo_object_toggle_visibility.py
+++ modules/operators/gizmo_object_toggle_visibility.py
@@ -3,14 +3,14 @@ from bpy.types import Operator
 from ..modifier_categories import have_gizmo_property
 
 
-class OBJECT_OT_ml_gizmo_object_toggle_visibility(Operator):
+class OBJECT_OT_ml_gizmo_object_(Operator):
     """The purpose of this operator is to get the opposite behaviour
     for the UI button to what the behaviour would be using the
     hide_viewport property.
     """
     bl_idname = "object.ml_gizmo_object_toggle_visibility"
     bl_label = "Toggle Gizmo Object Visibility"
-    bl_description = "Toggle Gizmo Object Visibility"
+    bl_description = "Toggle gizmo object visibility"
     bl_options = {'REGISTER', 'INTERNAL', 'UNDO'}
 
     def execute(self, context):
