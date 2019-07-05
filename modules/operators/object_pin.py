019745bd22b1ca18e744adac11799492e8d68ee5
Antti Tikka <antti.tikka@hotmail.com>
2019-07-06 01:22:52 +0300

    Cleanup: remove unnecessary parentheses

--- modules/operators/object_pin.py
+++ modules/operators/object_pin.py
@@ -5,7 +5,7 @@ from bpy.types import Operator
 class UI_OT_ml_object_pin(Operator):
     bl_idname = "ui.ml_object_pin"
     bl_label = "Pin Object"
-    bl_description = ("Pin/unpin the object")
+    bl_description = "Pin/unpin the object"
     bl_options = {'INTERNAL'}
 
     unpin: BoolProperty()
