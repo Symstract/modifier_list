a8d119b8267ce66cbed899e7b2ba6bb0e7e312a8
Antti Tikka <antti.tikka@hotmail.com>
2019-12-11 17:05:49 +0200

    Bump development version

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@
 bl_info = {
     "name": "Modifier List",
     "author": "Antti Tikka",
-    "version": (1, 5, 0),
+    "version": (1, 6, 0),
     "blender": (2, 80, 0),
     "location": "Properties Editor & View3D > Sidebar & View3D > Alt + Spacebar",
     "description": "Alternative UI layout for modifiers with handy features "
