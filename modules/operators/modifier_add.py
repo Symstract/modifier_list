0bdf53458a6bbbf94c75d448b15083b7d743faea
Antti Tikka <antti.tikka@hotmail.com>
2020-04-28 16:21:04 +0300

    Add poll for Add Modifier to take linking and overrides into account

--- modules/operators/modifier_add.py
+++ modules/operators/modifier_add.py
@@ -20,6 +20,11 @@ class OBJECT_OT_ml_modifier_add(Operator):
 
     modifier_type: StringProperty(options={'HIDDEN'})
 
+    @classmethod
+    def poll(cls, context):
+        ob = get_ml_active_object()
+        return ob.library is None or ob.override_library is not None
+
     def execute(self, context):
         ob = get_ml_active_object()
 
