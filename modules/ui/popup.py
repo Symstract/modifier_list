c2822dc779cfbfbef8b9f1137575f116ea0f136e
Antti Tikka <antti.tikka@hotmail.com>
2020-07-02 21:25:44 +0300

    Cleanup: unindent code

--- modules/ui/popup.py
+++ modules/ui/popup.py
@@ -38,44 +38,46 @@ class VIEW3D_OT_ml_modifier_popup(Operator):
 
         if not ob:
             layout.label(text="No active object")
+            return
         elif ob.type not in {'MESH', 'CURVE', 'SURFACE', 'FONT', 'LATTICE'}:
             layout.label(text="Wrong object type")
-        else:
-            wm = bpy.context.window_manager
-            popup_tab = wm.ml_popup_active_tab
+            return
+        
+        wm = bpy.context.window_manager
+        popup_tab = wm.ml_popup_active_tab
 
-            prefs = bpy.context.preferences.addons["modifier_list"].preferences
+        prefs = bpy.context.preferences.addons["modifier_list"].preferences
 
-            # Don't add a label when props_dialog is used, avoiding
-            # wasting space.
-            if not prefs.use_props_dialog:
-                row = layout.row()
-                pin_object_button(context, row)
-                row.label(text="Modifier Popup")
+        # Don't add a label when props_dialog is used, avoiding
+        # wasting space.
+        if not prefs.use_props_dialog:
+            row = layout.row()
+            pin_object_button(context, row)
+            row.label(text="Modifier Popup")
 
-            split_factor = self.panel_width / self.overall_width
-            split = layout.split(factor=split_factor)
+        split_factor = self.panel_width / self.overall_width
+        split = layout.split(factor=split_factor)
 
-            # === Content ===
-            col = split.column()
-            if popup_tab == 'MODIFIERS':
-                num_of_rows = prefs.mod_list_def_len
-                modifiers_ui(context, col, num_of_rows=num_of_rows, use_in_popup=True)
-            elif popup_tab == 'OBJECT_DATA':
-                vertex_groups_ui(context, col, num_of_rows=7)
+        # === Content ===
+        col = split.column()
+        if popup_tab == 'MODIFIERS':
+            num_of_rows = prefs.mod_list_def_len
+            modifiers_ui(context, col, num_of_rows=num_of_rows, use_in_popup=True)
+        elif popup_tab == 'OBJECT_DATA':
+            vertex_groups_ui(context, col, num_of_rows=7)
 
-            # === Tabs ===
-            col = split.column(align=True)
-            col.scale_y = 1.3
-            col.prop_tabs_enum(wm, "ml_popup_active_tab", icon_only=True)
+        # === Tabs ===
+        col = split.column(align=True)
+        col.scale_y = 1.3
+        col.prop_tabs_enum(wm, "ml_popup_active_tab", icon_only=True)
 
-            # When using the dialog type popup, the label is automatic
-            # and the pin button can't be put next to it. In that case,
-            # display it here.
-            if prefs.use_props_dialog:
-                col.separator(factor=3)
+        # When using the dialog type popup, the label is automatic
+        # and the pin button can't be put next to it. In that case,
+        # display it here.
+        if prefs.use_props_dialog:
+            col.separator(factor=3)
 
-                pin_object_button(context, col)
+            pin_object_button(context, col)
 
 
 def register():
