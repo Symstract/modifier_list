ea4846f13043a8ad389041f93979f1eb8a1c941e
Antti Tikka <antti.tikka@hotmail.com>
2020-07-08 22:15:48 +0300

    Add box_with_header template into ui_common

--- modules/ui/ui_common.py
+++ modules/ui/ui_common.py
@@ -1,3 +1,21 @@
+def box_with_header(layout, text, expand_data, expand_prop):
+    """Template for a header for boxes used as panels.
+    
+    Includes a triangle icon whose state is defined by expand_prop, a
+    property of expand_data.
+
+    Returns a box.
+    """
+    box = layout.box()
+
+    row = box.row()
+    icon = 'TRIA_DOWN' if getattr(expand_data, expand_prop) else 'TRIA_RIGHT'
+    row.prop(expand_data, expand_prop, icon=icon, text="", emboss=False)
+    row.label(text=text)
+
+    return box
+
+
 def favourite_modifiers_selection_layout(context, layout):
     ml_props = context.window_manager.modifier_list
     prefs = context.preferences.addons["modifier_list"].preferences
