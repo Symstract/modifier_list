6f64b2a20c115b65fb5924b244a1fec5885f4386
Antti Tikka <antti.tikka@hotmail.com>
2020-11-25 20:17:37 +0200

    Bump version to 1.6.2

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@
 bl_info = {
     "name": "Modifier List",
     "author": "Antti Tikka",
-    "version": (1, 6, 1),
+    "version": (1, 6, 2),
     "blender": (2, 81, 0),
     "location": "Properties Editor & View3D > Sidebar & View3D > Alt + Spacebar",
     "description": "Alternative UI layout for modifiers with handy features "
