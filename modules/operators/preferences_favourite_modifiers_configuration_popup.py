ace822c441a726aac3b4e021f341c180e51c1edc
Antti Tikka <antti.tikka@hotmail.com>
2021-02-25 03:20:33 +0200

    Add tooltip for Configure Favourite Modifiers

--- modules/operators/preferences_favourite_modifiers_configuration_popup.py
+++ modules/operators/preferences_favourite_modifiers_configuration_popup.py
@@ -6,6 +6,7 @@ from ..ui.ui_common import favourite_modifiers_configuration_layout
 class WM_OT_ml_favourite_modifiers_configuration_popup(Operator):
     bl_idname = "wm.ml_favourite_modifiers_configuration_popup"
     bl_label = "Configure Favourite Modifiers"
+    bl_description = "Choose and order your favourite modifiers"
     bl_options = {'INTERNAL'}
 
     def execute(self, context):
