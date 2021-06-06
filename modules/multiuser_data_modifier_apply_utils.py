7fe17056e4ea96667a2924fc5b98bf08af05484d
Antti Tikka <antti.tikka@hotmail.com>
2021-06-06 14:50:40 +0300

    Cleanup: add _ prefix to private method in LinkedObjectDataChanger

--- modules/multiuser_data_modifier_apply_utils.py
+++ modules/multiuser_data_modifier_apply_utils.py
@@ -6,10 +6,10 @@ class LinkedObjectDataChanger:
     """
     Helper class to be used when applying a modifier to one or all
     objects with multi-user data.
-    
+
     Usage:
-    
-    Use make_active_instance_data_unique to create a copy of the 
+
+    Use make_active_instance_data_unique to create a copy of the
     current data and to assing it to the active instance.
 
     Use assign_new_data_to_other_instances to assign the new data to the
@@ -23,7 +23,7 @@ class LinkedObjectDataChanger:
     def __init__(self, object):
         self._object = object
 
-    def get_correct_data_collection(self):
+    def _get_correct_data_collection(self):
         if self._object.type == 'MESH':
             return bpy.data.meshes
         elif self._object.type in {'CURVE', 'SURFACE', 'FONT'}:
@@ -40,7 +40,7 @@ class LinkedObjectDataChanger:
     def assign_new_data_to_other_instances(self):
         obs = bpy.data.objects
         obs_with_same_data = [ob for ob in obs if ob.data and ob.data.name == self._old_data_name]
-        data_collection = self.get_correct_data_collection()
+        data_collection = self._get_correct_data_collection()
 
         for ob in obs_with_same_data:
             ob.data = data_collection[self._new_data_name]
@@ -48,7 +48,7 @@ class LinkedObjectDataChanger:
         data_collection.remove(data_collection[self._old_data_name])
 
     def reassign_old_data_to_active_instance(self):
-        data_collection = self.get_correct_data_collection()
+        data_collection = self._get_correct_data_collection()
         self._object.data = data_collection[self._old_data_name]
 
         data_collection.remove(data_collection[self._new_data_name])
