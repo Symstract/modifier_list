45f53b3ea93dac6c2cba0eddc9034944e0dc834b
Antti Tikka <antti.tikka@hotmail.com>
2023-02-24 04:01:16 +0200

    Ensure there is active object in basic modifier operators
    This is to better support shortcuts.

--- modules/operators/modifier_move_up.py
+++ modules/operators/modifier_move_up.py
@@ -18,6 +18,10 @@ class OBJECT_OT_ml_modifier_move_up(Operator):
     @classmethod
     def poll(cls, ontext):
         ob = get_ml_active_object()
+
+        if not ob:
+            return False
+
         active_mod_index = ob.ml_modifier_active_index
         mods = ob.modifiers
 
